9b92c08ec8d8a0c23eed5df093ad58abcbf1338f
salimsaad-dot <salimsaad135@gmail.com>
2025-08-03 03:19:34 -0700

    Add files via upload

---
+++ InterfaceDemo.cs/InterfaceDemo.cs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DCIT318_Assignment2
+{
+    // Interface definition
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Interfaces ===");
+
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();        // Output: Car is moving
+            bicycle.Move();    // Output: Bicycle is moving
+        }
+    }
+}
